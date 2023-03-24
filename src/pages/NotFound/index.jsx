dbf8dc507d9cf6a20d5fddc905f67e874c74c98d
Larissa Olimpio <lrsolimpio@gmail.com>
2023-03-24 13:25:13 -0300

    criando o css da pagina NotFound

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,8 +1,18 @@
-function NotFound(){
-    return(
-        <>
-        <h2>Erro 404</h2>
-        </>
-    )
+import styles from "./NotFound.module.css";
+function NotFound() {
+  return (
+    <>
+      <div className={styles.background}></div>
+      <div className={styles.divErrorMensage}>
+        <span>Erro 404</span>
+        <h1>Página Não encontrada.</h1>
+        <p>Tem certeza de que era isso que estava procurando?</p>
+        <p>
+          Aguarde uns instantes e recarregue a página, ou volte para a página
+          inicial.
+        </p>
+      </div>
+    </>
+  );
 }
-export default NotFound
+export default NotFound;
