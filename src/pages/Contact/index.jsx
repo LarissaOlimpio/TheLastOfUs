d08b4d490f7e91d28840c7124f7dc4c8e000d2e4
Larissa Olimpio <lrsolimpio@gmail.com>
2023-03-27 12:32:48 -0300

    atualizando o css da página contato

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,37 +1,72 @@
-import TextField from 'components/TextField'
-import { useState } from 'react'
-import styles from './Contact.module.css'
+import Button from "components/Button";
+import TextField from "components/TextField";
+import { useState } from "react";
+import styles from "./Contact.module.css";
 
-function Contact(){
+function Contact() {
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+  const [email, setEmail] = useState("");
+  const [topic, setTopic] = useState("");
+  const [mensage, setMensage] = useState("");
 
-    const [name, setName] = useState('')
-    const [mensage, setMensage]=useState('')
+  return (
+    <section className={styles.section}>
+      <div className={styles.background}></div>
+      <form className={styles.form}>
+        <div className={styles.title}>
+          <h1>Gostaria de entrar em contato conosco?</h1>
+          <h2>
+            Preencha os campos abaixo que entraremos em contato assim que
+            possível
+          </h2>
+        </div>
 
-    return(
-      
-            <section>
-                <div className={styles.background}></div>
-                <form className={styles.form}>
-                    <div className={styles.title}>
-                        <h1>Gostaria de entrar em contato conosco?</h1>
-                        <h2>Preencha os campos abaixo que entraremos em contato assim que possível</h2>
-                    </div>
-                    <TextField
-                    label = "Nome"
-                    required= {true}
-                    placeholder= "Digite seu nome"
-                    value={name}
-                    modify= {value => setName(value)}
-                    />
-                    <TextField
-                    label="assunto"
-                    required={true}
-                    placeholder="Digite o assunto "
-                    value={mensage}
-                    modify={value => setMensage(value)}/>
-                </form>
-            </section>
-        
-    )
+        <TextField
+          label="Nome"
+          required={true}
+          placeholder="Digite seu nome"
+          value={name}
+          modify={(value) => setName(value)}
+        />
+
+        <TextField
+          label="Data"
+          required
+          placeholder="Data"
+          value={date}
+          modify={(value) => setDate(value)}
+        />
+
+        <TextField
+          label="E-mail"
+          required
+          placeholder="Digite seu email: contato@email.com"
+          value={email}
+          modify={(value) => setEmail(value)}
+        />
+
+        <TextField
+          label="assunto"
+          required
+          placeholder="Digite o assunto "
+          value={topic}
+          modify={(value) => setTopic(value)}
+        />
+
+        <TextField
+          label="Mensagem"
+          required
+          placeholder="Digite sua mensagem"
+          value={mensage}
+          modify={(value) => setMensage(value)}
+        />
+
+        <div className={styles.button}>
+          <Button>Enviar</Button>
+        </div>
+      </form>
+    </section>
+  );
 }
-export default Contact
+export default Contact;
